bd141f7e3c938b4765d0bf84f6161026eeb0e1e5
Maria Carvalhido <mizacarvalhido@gmail.com>
2025-03-03 11:24:43 -0300

    Oracle-One

---
+++ app.js
@@ -0,0 +1,23 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+function console(){
+     console.log('O botão foi clicado!')
+}
+
+function alerta(){
+     alert(`Eu amo JS!`)
+}
+
+function pronpt(){
+     let cidade = prompt('Digite alguma cidade brasileira:')
+     alert(`Estive em ${cidade} e lembrei de vc s2`)
+
+}
+
+function soma(){
+     let num1 = parseInt(prompt('Digite um numero inteiro:'))
+     let num2 = parseInt(prompt('Digite outro numero:'))
+     let sum = num1 + num2;
+     alert(`${num1} + ${num2} = ${sum}`)
+}
